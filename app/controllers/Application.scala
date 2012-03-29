50f07d5004d0093f7ecbd9b0817a6eb3b542e63a
Ryan Knight <ryan@grandcloud.com>
2012-03-29 16:21:11 -0600

    adding basic scala code input and output forms.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,32 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import views.html
 
 object Application extends Controller {
-  
+
+  val scalaCodeForm = Form(
+    single(
+      "inputScala" -> nonEmptyText
+    )
+  )
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(scalaCodeForm))
+  }
+
+  def evaluateScalaCode = Action {
+    implicit request =>
+      scalaCodeForm.bindFromRequest.fold(
+        errors => BadRequest,
+        {
+          case (inputScala) =>
+            val outputScala = inputScala.reverse;
+            Ok(html.output(outputScala))
+        }
+      )
   }
   
 }
